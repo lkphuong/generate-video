283848369b8c3fe28e717f56053361befa0806e6
lkphuong <lekhphuong@gmail.com>
2024-07-07 13:37:50 +0700

    refactor: Update file paths and add watermark text to videos

--- config.js
+++ config.js
@@ -1,17 +1,22 @@
-const VIDEO_INPUT = "./video.webm";
-const VIDEO_BACKUP = "./video.mp4";
-const PRODUCT = "./product.jpg";
+const VIDEO_INPUT = "./input/video.webm";
+const VIDEO_BACKUP = "./input/video.mp4";
+const PRODUCT = "./input/product.jpg";
+const AUDIO = "./input/audio.mp3";
 const VIDEO_DURATION = 10;
 const TIME_START = "00:00:00";
 const RAW_OUTPUT_PATH = "raw-output";
+const TMP_OUTPUT_PATH = "tmp-raw-output";
 const OUTPUT_PATH = "output";
+const TEXT = "./input/text.png";
 
 module.exports = {
   VIDEO_INPUT,
   VIDEO_BACKUP,
   PRODUCT,
+  AUDIO,
   VIDEO_DURATION,
   TIME_START,
   RAW_OUTPUT_PATH,
   OUTPUT_PATH,
+  TEXT,
 };
--- index.js
+++ index.js
@@ -11,6 +11,8 @@ const {
   VIDEO_BACKUP,
   RAW_OUTPUT_PATH,
   OUTPUT_PATH,
+  AUDIO,
+  TEXT,
 } = require("./config");
 
 ffmpeg.setFfmpegPath(ffmpegStatic);
@@ -61,6 +63,28 @@ const addWatermarkProduction = (inputFile, outputFile, watermarkFile) => {
   });
 };
 
+const addWatermarkText = (inputFile, outputFile, watermarkFile) => {
+  return new Promise((resolve, reject) => {
+    ffmpeg(inputFile)
+      .input(watermarkFile)
+      .input(AUDIO)
+      .duration(10)
+      .complexFilter([
+        "[1:v]scale=500:500[watermark]",
+        "[0:v][watermark]overlay=(main_w-overlay_w)/2:(main_h-overlay_h)/2:enable='between(t,0,8)'", // Apply the overlay filter from the 8th second onwards and position it in the center
+      ])
+      .audioCodec("copy")
+      .on("error", function (err) {
+        console.log("An error occurred: " + err.message);
+        reject(err);
+      })
+      .on("end", function () {
+        resolve();
+      })
+      .save(outputFile);
+  });
+};
+
 const incrementInSeconds = (time, incrementInSeconds) => {
   // Convert the time string to a Date object
   const parts = time.split(":");
@@ -82,6 +106,7 @@ const incrementInSeconds = (time, incrementInSeconds) => {
 
 const initGenerateVideo = async () => {
   const videoExist = fs.existsSync(VIDEO_INPUT);
+
   let videoPath = VIDEO_INPUT;
   if (!videoExist) {
     videoPath = VIDEO_BACKUP;
@@ -110,13 +135,19 @@ const initGenerateVideo = async () => {
       console.log("Split video: ", time);
 
       //#region add watermark product
-      const outputPatternWatermark = join(OUTPUT_PATH, `${i + 1}.mp4`);
+      const outputPatternWatermark = join(RAW_OUTPUT_PATH, `tmp-${i + 1}.mp4`);
       await addWatermarkProduction(
         outputPattern,
         outputPatternWatermark,
         PRODUCT
       );
-      console.log("Add watermark: ", OUTPUT_PATH);
+      console.log("Add watermark: ", RAW_OUTPUT_PATH);
+
+      const outputPatternText = join(OUTPUT_PATH, `${i + 1}.mp4`);
+      await addWatermarkText(outputPatternWatermark, outputPatternText, TEXT);
+
+      //clear tmp file
+      fs.unlinkSync(outputPatternWatermark);
     } catch (error) {
       console.log("Error: ", error);
 
